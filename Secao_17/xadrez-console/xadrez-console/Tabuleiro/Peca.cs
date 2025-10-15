c43cdd8b948c489989555e2d3ac5d677e720b513
J0A0Natan <joao@prymelab.com.br>
2025-10-15 15:11:38 -0300

    Começando a implementar o tabuleiro

---
+++ Secao_17/xadrez-console/xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using tabuleiro;
+
+namespace Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovimentos { get; set; }
+        public Tabuleiro1 Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro1 tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tabuleiro = tabuleiro;
+            QteMovimentos = 0;
+        }
+    }
+}
---
+++ Secao_17/xadrez-console/xadrez-console/Tabuleiro/Tabuleiro1.cs
@@ -0,0 +1,18 @@
+﻿using Tabuleiro;
+
+namespace tabuleiro
+{
+    internal class Tabuleiro1
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
+
+        public Tabuleiro1(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
+    }
+}
