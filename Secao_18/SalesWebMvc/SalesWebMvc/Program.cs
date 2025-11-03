b5486c5d57ee91a5a2a245002cc84b2b48b87067
J0A0Natan <joao@prymelab.com.br>
2025-11-03 17:25:19 -0300

    Localização de números e datas

--- Secao_18/SalesWebMvc/SalesWebMvc/Program.cs
+++ Secao_18/SalesWebMvc/SalesWebMvc/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -50,6 +52,15 @@ else
 }
 
 // 5️ Configuração padrão do pipeline HTTP
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
+app.UseRequestLocalization(localizationOptions);
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
